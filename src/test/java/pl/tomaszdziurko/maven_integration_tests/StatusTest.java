8fa417aa2619770004cdedd413b05571173b53b0
Pavel Sadikov <psadikov@bitsighttech.com>
2015-02-26 13:09:46 -0500

    Renaming class

--- src/test/java/pl/tomaszdziurko/maven_integration_tests/StatusTest.java
+++ src/test/java/pl/tomaszdziurko/maven_integration_tests/StatusTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-public class StatusTestIT {
+public class StatusTest {
 
     @Test
     public void shouldFail() {
